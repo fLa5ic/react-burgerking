f574771b366666ac9d0a73436c462953897a3b10
MaximPasedko <9911273@mail.ru>
2025-09-29 11:29:56 +0300

    Fix GitHub Pages routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import App from './App';
 
@@ -12,10 +12,10 @@ const rootElem = document.getElementById('root');
 if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
   root.render(
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
   );
 }
